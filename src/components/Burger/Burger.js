201829624b01caad0247bba7663e2087b723af05
Neha Patel <neha.r.p@gmail.com>
2020-05-30 16:59:17 -0700

    part 132 complete

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,25 @@ import Ingredient from './Indredient/Ingredient'
 import'./Burger.css'
 
 const Burger = props => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <Ingredient key={igKey + i} type={igKey} />
+            })
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredients!</p>
+        }
+
+    
     return (
         <div className="Burger">
             <Ingredient type="bread-top" />
-            <Ingredient type="cheese" />
-            <Ingredient type="meat" />
+            {transformedIngredients}
             <Ingredient type="bread-bottom" />
         </div>
     )
