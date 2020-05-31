368d89f1845210ee08787ad0e53c418517338df6
Neha Patel <neha.r.p@gmail.com>
2020-05-30 20:32:44 -0700

    add and remove ingredients working, price total is working, burger image is building correctly

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,13 +3,21 @@ import React from 'react'
 import './BuildControl.css'
 
 const BuildControl = props => {
-    return (
-        <div className="BuildControl">
-            <div className="Label">{props.label}</div>
-            <button className="Less">Less</button>
-            <button className="More">More</button>
-        </div>
-    )
+  return (
+    <div className='BuildControl'>
+      <div className='Label'>{props.label}</div>
+      <button
+        className='Less'
+        onClick={props.removed}
+        disabled={props.disabled}
+      >
+        Less
+      </button>
+      <button className='More' onClick={props.added}>
+        More
+      </button>
+    </div>
+  )
 }
 
 export default BuildControl
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,16 +4,25 @@ import './BuildControls.css'
 import BuildControl from './BuildControl/BuildControl'
 
 const controls = [
-    { label: 'Salad', type: 'salad' },
-    { label: 'Bacon', type: 'bacon' },
-    { label: 'Cheese', type: 'cheese' },
-    { label: 'Meat', type: 'meat' }
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
 ]
 
 const BuildControls = props => {
   return (
-    <div className="BuildControls">
-        {controls.map( ctrl => <BuildControl key={ctrl.label} label={ctrl.label} />)}
+    <div className='BuildControls'>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+      {controls.map(ctrl => (
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
+        />
+      ))}
     </div>
   )
 }
